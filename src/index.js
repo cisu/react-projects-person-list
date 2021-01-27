433d3044ab000774bcb9bbee72b1d466e76fc326
cisu <49948070+cisu@users.noreply.github.com>
2021-01-27 23:37:31 +0200

    person-list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const Person = ({img, name, job, children}) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return (
+    <article className='person'>
+      <img src={url} alt='peron' />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
+
+const PersonList = () => {
+  return (
+    <section className='person-list'>
+      <Person img='34' name='john' job='developer' />
+      <Person img='22' name='xaris' job='artist'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          amet odit reprehenderit dicta? Enim repellat laborum quaerat hic eius?
+        </p>
+      </Person>
+      <Person img='65' name='george' job='super developer' />
+    </section>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PersonList />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
